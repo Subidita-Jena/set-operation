7ffc2f8d00282e40c35409ea742b86ce4147a5d6
Subidita-Jena <90219666+Subidita-Jena@users.noreply.github.com>
2021-09-07 23:36:09 +0530

    circle and extension

---
+++ circle.py
@@ -0,0 +1,9 @@
+#r = radius of the circle
+''' input the radius of the circle = 1.1
+    the area of the circle with radius 1.1 is
+    3.8013271108436504'''
+
+r = 1.1
+pi = 3.141592653589793
+print("Area of the circle",pi*r*r)
+
---
+++ extension.py
@@ -0,0 +1,3 @@
+file_name = "abc.py"
+f_extension = file_name.split('.')
+print("The extension of the file is: " + (f_extension[-1]))
